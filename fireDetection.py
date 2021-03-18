0b703e9d712ad0ae157b8f6c5739bbfa6e1d33ff
darrenchay <darrenchay@gmail.com>
2021-03-18 17:56:28 -0400

    implemenented first 3 masks

--- fireDetection.py
+++ fireDetection.py
@@ -15,8 +15,8 @@ import time
 Tk().withdraw()
 filename = askopenfilename()
 print("filename: ", filename)
-originalImg = cv.imread(filename, -1)  # reading image 
-img = np.float32(originalImg)/255
+img = cv.imread(filename, -1)  # reading image 
+# img = np.float32(img)/255
 
 # Convert from BGR to Lab format
 imgLab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
@@ -32,29 +32,49 @@ print(np.mean(L))
 print(np.mean(A))
 print(np.mean(B))
 
-maskR1 = cv.inRange(imgLab, int(Lmean), 100)
-result = cv.bitwise_and(img, img, mask=maskR1)
+print(np.amax(L))
+print(np.amax(A))
+print(np.amax(B))
+# Extracting R1 Mask
+lowerBound = np.array([int(Lmean), int(np.min(A)), int(np.min(B))])
+upperBound = np.array([int(np.max(L)), int(np.max(A)), int(np.max(B))])
+maskR1 = cv.inRange(imgLab, lowerBound, upperBound)
+result1 = cv.bitwise_and(imgLab, imgLab, mask=maskR1)
+# cv.imshow("Mask R1 Result",result)
+# plt.subplot(1, 2, 1)
+# plt.imshow(maskR1, cmap="gray")
+# plt.subplot(1, 2, 2)
+# plt.imshow(result1)
+# plt.show()
 
-plt.subplot(1, 2, 1)
+# Extracting R2 Mask
+lowerBound = np.array([int(np.min(L)), int(Amean), int(np.min(B))])
+upperBound = np.array([int(np.max(L)), int(np.max(A)), int(np.max(B))])
+maskR2 = cv.inRange(imgLab, lowerBound, upperBound)
+result2 = cv.bitwise_and(imgLab, imgLab, mask=maskR2)
+# cv.imshow("Mask R1 Result",result)
+# plt.subplot(1, 2, 1)
+# plt.imshow(maskR2, cmap="gray")
+# plt.subplot(1, 2, 2)
+# plt.imshow(result2)
+# plt.show()
+
+# Extracting R3 Mask
+lowerBound = np.array([int(np.min(L)), int(np.min(A)), int(Bmean)])
+upperBound = np.array([int(np.max(L)), int(np.max(A)), int(np.max(B))])
+maskR3 = cv.inRange(imgLab, lowerBound, upperBound)
+result3 = cv.bitwise_and(imgLab, imgLab, mask=maskR3)
+# cv.imshow("Mask R1 Result",result)
+plt.subplot(1, 3, 1)
 plt.imshow(maskR1, cmap="gray")
-plt.subplot(1, 2, 2)
-plt.imshow(result)
+plt.subplot(1, 3, 2)
+plt.imshow(maskR2, cmap="gray")
+plt.subplot(1, 3, 3)
+plt.imshow(maskR3, cmap="gray")
 plt.show()
 
 
-# plt.subplot(121),plt.imshow(L),plt.title('L')
-# plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(A),plt.title('a')
-# plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(B),plt.title('b')
-# plt.xticks([]), plt.yticks([])
-# plt.show()
-# cv.imshow("L_Channel",L) # For L Channel
-# cv.imshow("A_Channel",A) # For A Channel (Here's what You need)
-# cv.imshow("B_Channel",B) # For B Channel
-# cv.waitKey(0)
-# cv.destroyAllWindows()
+
 imgRGB = cv.cvtColor(imgLab, cv.COLOR_LAB2RGB)
 # plt.imshow(imgRGB)
 # plt.show()
-exit()
