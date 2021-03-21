5a409842658f599ec600d29a3216f37eeac2f88d
darrenchay <darrenchay@gmail.com>
2021-03-21 15:04:26 -0400

    finished metric implementation

--- skinAlgorithms.py
+++ skinAlgorithms.py
@@ -4,7 +4,11 @@ ID: 1049254
 Course: CIS*4720
 Assignment 3
 
-Description: 
+Description: This file prompts the user to choose an image file which will be checked to see if their is any
+             skin present in the image using one of the 3 algorithms implemented below. After choosing the image, 
+             the user will be prompted to choose which algorithm will be used and the resulting image will be stored
+             in the Results folder
+
 """
 # Image Processing Libraries
 import cv2 as cv
@@ -21,7 +25,7 @@ import time
 
 # ALGORITHMS
 
-
+# This algorithm by Peer et al. uses the RGB color space to detect skin based on several masks 
 def peerAlgo(img):
     # Convert from BGR to RGB format
     imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
@@ -78,125 +82,111 @@ def peerAlgo(img):
         # plt.imshow(maskR4, cmap="gray")
         # plt.subplot(1, 5, 5)
         # plt.imshow(filteredImg)
-
-        # # Saving processed image
-        # newFileName = 'Results/rgbColorSpace' + \
-        # filenameSplit[len(filenameSplit)-1]
-        # cv.imwrite(newFileName, filteredImg)
-        # plt.show()
     return filteredImg
 
 
-def cvtRGB2HSI(img):
-    # pre_proc
-    rows, cols = img.shape[0], img.shape[1]
-    # transpose to 3 * pixels_number
-    # utilized for extract R/G/B cols
-    t_p = img.swapaxes(0, 2).swapaxes(1, 2)
-    R, G, B = t_p[0], t_p[1], t_p[2]
-    dRG, dRB, dGB = R - G, R - B, G - B  # all belongs to [0,255]
-    dRG[dRG == 0.0] = 0.5
-    dRB[dRB == 0.0] = 0.5
-    dGB[dGB == 0.0] = 0.5  # just ignore my divide_zero exception handler
-    # H part
-    cos = (dRG+dRB)/(2*np.sqrt(dRG**2+dRB*dGB))
-    H = np.arccos(cos)
-    H[np.isnan(H)] = 0.0  # if cos is too big, arccos returns nan
-    # I part
-    I = img.mean(axis=2)
-    # prepare for Saturation calc
-    Imin = img.min(axis=2)
-    I[I == 0.0] = 1
-    # S part
-    S = 1 - Imin / I
-    return H,S,I
+# def cvtRGB2HSI(img):
+#     # pre_proc
+#     rows, cols = img.shape[0], img.shape[1]
+#     # transpose to 3 * pixels_number
+#     # utilized for extract R/G/B cols
+#     t_p = img.swapaxes(0, 2).swapaxes(1, 2)
+#     R, G, B = t_p[0], t_p[1], t_p[2]
+#     dRG, dRB, dGB = R - G, R - B, G - B  # all belongs to [0,255]
+#     dRG[dRG == 0.0] = 0.5
+#     dRB[dRB == 0.0] = 0.5
+#     dGB[dGB == 0.0] = 0.5  # just ignore my divide_zero exception handler
+#     # H part
+#     cos = (dRG+dRB)/(2*np.sqrt(dRG**2+dRB*dGB))
+#     H = np.arccos(cos)
+#     H[np.isnan(H)] = 0.0  # if cos is too big, arccos returns nan
+#     # I part
+#     I = img.mean(axis=2)
+#     # prepare for Saturation calc
+#     Imin = img.min(axis=2)
+#     I[I == 0.0] = 1
+#     # S part
+#     S = 1 - Imin / I
+#     return H,S,I
 
 
-def linAlgo(img):
+# def linAlgo(img):
 
-    # Convert from BGR to HSI format
-    img = cv.cvtColor(img, cv.COLOR_BGR2HLS)
-    filteredImg = np.zeros((rows, cols), dtype=np.float32)
+#     # Convert from BGR to HSI format
+#     img = cv.cvtColor(img, cv.COLOR_BGR2HLS)
+#     filteredImg = np.zeros((rows, cols), dtype=np.float32)
 
-    H, S, I  = cvtRGB2HSI(img)
-    # Split the H, S, & I channels
-    hChannel, sChannel, iChannel = cv.split(img)
-    print(hChannel, sChannel, iChannel)
-    # Getting max and mins of each channel
-    maxH = int(np.max(hChannel))
-    minH = int(np.min(hChannel))
+#     H, S, I  = cvtRGB2HSI(img)
+#     # Split the H, S, & I channels
+#     hChannel, sChannel, iChannel = cv.split(img)
+#     print(hChannel, sChannel, iChannel)
+#     # Getting max and mins of each channel
+#     maxH = int(np.max(hChannel))
+#     minH = int(np.min(hChannel))
 
-    maxS = int(np.max(sChannel))
-    minS = int(np.min(sChannel))
-    print("maxS", maxS)
+#     maxS = int(np.max(sChannel))
+#     minS = int(np.min(sChannel))
+#     print("maxS", maxS)
 
-    maxI = int(np.max(iChannel))
-    minI = int(np.min(iChannel))
+#     maxI = int(np.max(iChannel))
+#     minI = int(np.min(iChannel))
 
-    # Creating R1 Mask (I > 40)
-    lowerBound = np.array((minH, minS, 40))
-    upperBound = np.array((maxH, maxS, maxI))
-    print("Bounds")
-    # pprint.pprint(lowerBound)
-    maskR1 = cv.inRange(img, lowerBound, upperBound)
-    print("Mask R1")
-    pprint.pprint(maskR1)
+#     # Creating R1 Mask (I > 40)
+#     lowerBound = np.array((minH, minS, 40))
+#     upperBound = np.array((maxH, maxS, maxI))
+#     print("Bounds")
+#     # pprint.pprint(lowerBound)
+#     maskR1 = cv.inRange(img, lowerBound, upperBound)
+#     print("Mask R1")
+#     pprint.pprint(maskR1)
 
-    # Creating R2 part a Mask (13 < S < 110 & 0° < H < 28°)
-    lowerBound = np.array((0, 13, minI))
-    upperBound = np.array((28, 110, maxI))
-    maskR2_a = cv.inRange(img, lowerBound, upperBound)
-    # print("Mask R2")
-    # pprint.pprint(maskR2)
+#     # Creating R2 part a Mask (13 < S < 110 & 0° < H < 28°)
+#     lowerBound = np.array((0, 13, minI))
+#     upperBound = np.array((28, 110, maxI))
+#     maskR2_a = cv.inRange(img, lowerBound, upperBound)
+#     # print("Mask R2")
+#     # pprint.pprint(maskR2)
 
-    # Creating R2 part b Mask (13 < S < 110 & 332° < H < 360°)
-    lowerBound = np.array((332, 13, minI))
-    upperBound = np.array((360, 110, maxI))
-    maskR2_b = cv.inRange(img, lowerBound, upperBound)
-    # print("Mask R3")
-    # pprint.pprint(maskR3)
+#     # Creating R2 part b Mask (13 < S < 110 & 332° < H < 360°)
+#     lowerBound = np.array((332, 13, minI))
+#     upperBound = np.array((360, 110, maxI))
+#     maskR2_b = cv.inRange(img, lowerBound, upperBound)
+#     # print("Mask R3")
+#     # pprint.pprint(maskR3)
 
-    # Creating R4 Mask (13 < S < 75 & 309° < H < 331°)
-    lowerBound = np.array((309, 13, minI))
-    upperBound = np.array((331, 75, maxI))
-    maskR3 = cv.inRange(img, lowerBound, upperBound)
-    # print("Mask R3")
-    # pprint.pprint(maskR3)
+#     # Creating R4 Mask (13 < S < 75 & 309° < H < 331°)
+#     lowerBound = np.array((309, 13, minI))
+#     upperBound = np.array((331, 75, maxI))
+#     maskR3 = cv.inRange(img, lowerBound, upperBound)
+#     # print("Mask R3")
+#     # pprint.pprint(maskR3)
 
-    # Applying all the masks to the image
-    filteredImg = cv.bitwise_and(img, img, mask=maskR1)
-    maskR2 = cv.bitwise_or(maskR2_a, maskR2_b)
-    filteredImg = cv.bitwise_and(filteredImg, filteredImg, mask=maskR2)
-    filteredImg = cv.bitwise_and(filteredImg, filteredImg, mask=maskR3)
+#     # Applying all the masks to the image
+#     filteredImg = cv.bitwise_and(img, img, mask=maskR1)
+#     maskR2 = cv.bitwise_or(maskR2_a, maskR2_b)
+#     filteredImg = cv.bitwise_and(filteredImg, filteredImg, mask=maskR2)
+#     filteredImg = cv.bitwise_and(filteredImg, filteredImg, mask=maskR3)
 
-    plt.subplot(1, 5, 1)
-    plt.imshow(maskR1, cmap="gray")
-    plt.subplot(1, 5, 2)
-    plt.imshow(maskR2_a, cmap="gray")
-    plt.subplot(1, 5, 3)
-    plt.imshow(maskR2_b, cmap="gray")
-    plt.subplot(1, 5, 4)
-    plt.imshow(maskR3, cmap="gray")
-    plt.subplot(1, 5, 5)
-    plt.imshow(filteredImg)
-    plt.show()
+#     plt.subplot(1, 5, 1)
+#     plt.imshow(maskR1, cmap="gray")
+#     plt.subplot(1, 5, 2)
+#     plt.imshow(maskR2_a, cmap="gray")
+#     plt.subplot(1, 5, 3)
+#     plt.imshow(maskR2_b, cmap="gray")
+#     plt.subplot(1, 5, 4)
+#     plt.imshow(maskR3, cmap="gray")
+#     plt.subplot(1, 5, 5)
+#     plt.imshow(filteredImg)
+#     plt.show()
 
-    filteredImg = cv.cvtColor(filteredImg, cv.COLOR_BGR2RGB)
-    return filteredImg
+#     filteredImg = cv.cvtColor(filteredImg, cv.COLOR_BGR2RGB)
+#     return filteredImg
 
-
+# This algorithm by Chai et al. uses the YCrCb color space to detect skin based on several masks 
 def chaiAlgo(img):
     # Convert from BGR to YCbCr colorspace
     imgYCbCr = cv.cvtColor(img, cv.COLOR_BGR2YCR_CB)
-    # cv.imwrite("testingYCbCr.png", imgYCbCr)
     filteredImg = np.zeros((rows, cols))
-
-    # Split the Y, Cb, & Cr channels
-    # yChannel, CrChannel, CbChannel = cv.split(img)
-    # pprint.pprint(yChannel)
-    # pprint.pprint(CrChannel)
-    # pprint.pprint(CbChannel)
-    # print(np.max(yChannel), np.max(CbChannel), np.max(CrChannel))
 
     # Creating R1 Mask (77 ≤ Cb ≤ 127)
     lowerBound = np.array((0, 0, 77))
@@ -210,6 +200,7 @@ def chaiAlgo(img):
     upperBound = np.array((255, 173, 255))
     maskR2 = cv.inRange(imgYCbCr, lowerBound, upperBound)
 
+    # Applying masks to image
     filteredImg = cv.bitwise_and(imgYCbCr, imgYCbCr, mask=maskR1)
     filteredImg = cv.bitwise_and(filteredImg, filteredImg, mask=maskR2)
     filteredImg = cv.cvtColor(filteredImg, cv.COLOR_YCR_CB2BGR)
@@ -218,6 +209,7 @@ def chaiAlgo(img):
     filteredImg[np.all(filteredImg == (0, 135, 0), axis=-1)] = (0, 0, 0)
     return filteredImg
 
+# This algorithm by Wang & Yuan uses the HSV and normalized RGB color space to detect skin based on several masks from both color spaces
 def wangAlgo(img):
     # Convert from BGR to HSV colorspace
     imgHSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -226,98 +218,50 @@ def wangAlgo(img):
     lowerBound = np.array((0, 51, 89))
     upperBound = np.array((25, 173, 255))
     maskR1 = cv.inRange(imgHSV, lowerBound, upperBound)
-    cv.imwrite("maskR1.png", maskR1)
+    # cv.imwrite("maskR1.png", maskR1)
     
-    # Creating R2 Mask ()
+    # Creating R2 Mask (0.36 < r < 0.465 & 0.28 < g < 0.363)
     imgRGB = normalized(img)
     lowerBound = np.array((0, 71, 92))
-    # lowerBound = np.array((0, 28, 36))
     upperBound = np.array((255, 93, 119))
-    # upperBound = np.array((1000, 363, 465))
     maskR2 = cv.inRange(imgRGB, lowerBound, upperBound)
-
-    # maskR2[valid_range] = 255
-    # maskR2[np.logical_not(valid_range)] = 0
-    cv.imwrite("maskR2.png", maskR2)
+    # cv.imwrite("maskR2.png", maskR2)
     
+    # Applying mask R1
     filteredImgHSV = cv.bitwise_and(imgHSV, imgHSV, mask=maskR1)
-    cv.imwrite("HSV.png", filteredImgHSV)
-    
+    # cv.imwrite("HSV.png", filteredImgHSV)
+    # Converting back to BGR color space
     filteredImgRGB = cv.cvtColor(filteredImgHSV, cv.COLOR_HSV2BGR)
-    cv.imwrite("convertedRGB.png", filteredImgRGB)
-    
+    # Apply mask R2
     filteredImgRGB = cv.bitwise_and(filteredImgRGB, filteredImgRGB, mask=maskR2)
-    cv.imwrite("endImg.png", filteredImgRGB)
     
     return filteredImgRGB
-    # normalized(img)
 
-# def normalized(img):
-    
-#     # imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-#     norm=np.zeros((img.shape[0],img.shape[1],3),np.float32)
-#     norm_rgb=np.zeros((img.shape[0],img.shape[1],3),np.uint8)
-#     sum = np.zeros((img.shape[0],img.shape[1]), np.int32)
-    
-#     # Split the R, G, & B channels
-#     rChannel, gChannel, bChannel = cv.split(img)
-    
-#     print("First Pixel")
-#     pprint.pprint(rChannel[0][0])
-#     pprint.pprint(gChannel[0][0])
-#     pprint.pprint(bChannel[0][0])
-    
-#     print(rChannel.dtype)
-    
-#     # Getting the sum
-#     print("Sum", int(rChannel[0][0]) + int(gChannel[0][0]) + int(bChannel[0][0]))
-#     sumChannels = rChannel.astype(int) + gChannel.astype(int) + bChannel.astype(int)
-#     print("Sum")
-#     pprint.pprint(sumChannels)
-#     rChannel = rChannel/sumChannels 
-#     gChannel = gChannel/sumChannels 
-#     bChannel = 1 - rChannel - gChannel
-#     print("rChannel normalized")
-#     pprint.pprint(rChannel)
-    
-    
-#     # Scaling
-#     rChannel *= 255
-#     gChannel *= 255
-#     bChannel *= 255
-#     print("rCHannel scaled")
-#     pprint.pprint(rChannel)
-    
-#     norm[:,:,0] = rChannel
-#     norm[:,:,1] = gChannel
-#     norm[:,:,2] = bChannel
-#     cv.imwrite("normalizedUnscaled.png", norm)
-
-#     norm_rgb=cv.convertScaleAbs(norm)
-#     pprint.pprint(norm_rgb)
-#     cv.imwrite("normalized.png", norm_rgb)
-#     return norm_rgb
-
+# This function converts an image in the RGB color space to the normalized RGB colorspace (rg chrominance)
 def normalized(img):
-        # imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        # Creating the normalized img
         norm=np.zeros((img.shape[0],img.shape[1],3),np.float32)
         norm_rgb=np.zeros((img.shape[0],img.shape[1],3),np.uint8)
 
+        # Getting the 3 color channels
         b=img[:,:,0]
         g=img[:,:,1]
         r=img[:,:,2]
 
+        # Getting the sum of RGB values and storing them as ints
         sum=b.astype(int) + g.astype(int) + r.astype(int)
         # pprint(sum)
 
+        # Normalizing and scaling values of rgb to 255
         norm[:,:,0]=b/sum*255.0
         norm[:,:,1]=g/sum*255.0
         norm[:,:,2]=r/sum*255.0
 
+        # Normalizing to rgb
         norm_rgb=cv.convertScaleAbs(norm)
-        pprint.pprint(norm_rgb)
-        cv.imwrite("normalized.png", norm_rgb)
+        # pprint.pprint(norm_rgb)
+        # cv.imwrite("normalized.png", norm_rgb)
         return norm_rgb
   
 # Reading the file
@@ -338,14 +282,14 @@ rows, cols, extra = img.shape
 
 # Choosing which algorithm to run
 algorithmNo = 0
-while int(algorithmNo) not in [1, 2, 3, 4]:
+while int(algorithmNo) not in [1, 2, 3]:
     print("******************************************")
     print("\t\tAlgorithms:")
     print("******************************************")
     print("Peer et al. (RGB color space):\t\t", 1)
-    print("Lin et al. (HSI color space):\t\t", 2)
-    print("Chai & Ngan (YCbCr color space):\t", 3)
-    print("Wang & Yuan (HSV & normalized RGB):\t", 4)
+    print("Chai & Ngan (YCbCr color space):\t", 2)
+    print("Wang & Yuan (HSV & normalized RGB):\t", 3)
+    # print("Lin et al. (HSI color space):\t\t", 4)
     print("******************************************")
     algorithmNo = input("Enter the number of the algorithm you want to run: ")
     if not algorithmNo.isnumeric():
@@ -365,31 +309,29 @@ if algorithmNo is 1:
         filenameSplit[len(filenameSplit)-1]
     print("Output File: ", newFileName)
 
-    print("====================\nRunning Peer et al. Algorithm\n====================")
+    print("=========================\nRunning Peer et al. Algorithm\n=========================")
     processedImage = peerAlgo(img)
 elif algorithmNo is 2:
-
-    newFileName = 'Results/lin-output-' + \
-        filenameSplit[len(filenameSplit)-1]
-    print("Output File: ", newFileName)
-
-    print("====================\nRunning Lin et al. Algorithm\n====================")
-    processedImage = linAlgo(img)
-elif algorithmNo is 3:
-
     newFileName = 'Results/chai-output-' + \
         filenameSplit[len(filenameSplit)-1]
     print("Output File: ", newFileName)
 
-    print("====================\nRunning Chai & Ngan Algorithm\n====================")
+    print("=========================\nRunning Chai & Ngan Algorithm\n=========================")
     processedImage = chaiAlgo(img)
-else:
+elif algorithmNo is 3:
     newFileName = 'Results/wang-output-' + \
         filenameSplit[len(filenameSplit)-1]
     print("Output File: ", newFileName)
 
-    print("====================\nRunning Wang & Yuan Algorithm\n====================")
+    print("=========================\nRunning Wang & Yuan Algorithm\n=========================")
     processedImage = wangAlgo(img)
+else:
+    newFileName = 'Results/lin-output-' + \
+        filenameSplit[len(filenameSplit)-1]
+    print("Output File: ", newFileName)
+
+    print("=========================\nRunning Lin et al. Algorithm\n=========================")
+    # processedImage = linAlgo(img)
 
 # Ending timer
 endTime = time.time()
---
+++ skinDetectionMetric.py
@@ -0,0 +1,94 @@
+""" 
+Name: Darren Chay Loong
+ID: 1049254
+Course: CIS*4720
+Assignment 3
+
+Description: This file helps measure the percentage of true positive that is present 
+             in the processed images by using a ground truth image as basis
+
+"""
+# Image Processing Libraries
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
+from scipy import stats
+
+# Utility Libraries
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import pprint
+import time
+
+# Extracting the skin section from the ground truth images
+# Reading the ground truth file
+Tk().withdraw()
+filename = askopenfilename()
+# print("filename: ", filename)
+
+# Check if file was selected
+if not filename:
+    print("error: no file selected")
+    exit(1)
+
+# Reading Image
+grndTruthImg = cv.imread(filename, -1)
+# img = np.float32(img)/255
+print("Image dimensions: ")
+pprint.pprint(grndTruthImg.shape)
+rows, cols, extra = grndTruthImg.shape
+
+
+# Creating the mask from the ground truth image
+lowerBound = np.array((0, 0, 0))
+upperBound = np.array((235, 235, 255)) # Removing all white-ish pixels from ground truth image
+mask = cv.inRange(grndTruthImg, lowerBound, upperBound)
+maskedTruthImg = cv.bitwise_and(grndTruthImg, grndTruthImg, mask=mask)
+# cv.imwrite("testingGrndTruth.png", maskedTruthImg)
+
+# Saving the count of pixels that are skin pixels and not skin pixels
+skinPixelCount = np.count_nonzero(np.all(maskedTruthImg != (0, 0, 0), axis=-1))
+print("Skin pixels: ",skinPixelCount)
+notSkinPixelCount = (rows*cols) - skinPixelCount
+print("Not skin pixels: ", notSkinPixelCount)
+
+# Comparing ground truth with mask
+
+# Retreiving processed image
+Tk().withdraw()
+filename = askopenfilename()
+# print("filename: ", filename)
+
+# Check if file was selected
+if not filename:
+    print("error: no file selected")
+    exit(1)
+
+# Reading processed image
+skinImg = cv.imread(filename, -1)
+
+# Applying ground truth mask to processed image
+processedMaskedImg = cv.bitwise_and(skinImg, skinImg, mask=mask)
+# cv.imwrite("testingProcessedImgMask.png", processedMaskedImg)
+
+# Counting num of pixels that are stil present
+processedSkinImgPixelCount = np.count_nonzero(np.all(processedMaskedImg != (0, 0, 0), axis=-1))
+print("Skin pixels present in processed image:", processedSkinImgPixelCount)
+# print("Not skin pixels: ", np.count_nonzero(np.all(processedMaskedImg == (0, 0, 0), axis=-1)))
+
+print("True positive %: ", round(processedSkinImgPixelCount/skinPixelCount * 100, 3))
+
+
+# Extracting false positive
+falsePosImg = cv.bitwise_xor(skinImg, processedMaskedImg)
+# cv.imwrite("testingFP.png", falsePosImg)
+
+# Counting false positive pixels in processed image
+falsePosSkinCount = np.count_nonzero(np.all(falsePosImg != (0, 0, 0), axis=-1))
+print("Non skin pixels present in processed image:", falsePosSkinCount)
+# print("Not skin pixels: ", np.count_nonzero(np.all(falsePosImg == (0, 0, 0), axis=-1)))
+
+print("False positive %: ", round(falsePosSkinCount/notSkinPixelCount * 100, 3))
+
+
